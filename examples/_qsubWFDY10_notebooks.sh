03c09b22aba97d24e42dd1ecfbdc673fe895e43f
Tarek Allam <t.allam.jr@gmail.com>
2018-11-20 14:32:48 +0000

    Adding output and results from cadence analysis

---
+++ examples/_qsubWFDY10_notebooks.sh
@@ -0,0 +1,25 @@
+# qsub -v NOTEBOOK=example_cadence_alt_sched_wfd.ipynb -N nb_alt_sched_wfd_Y10 jobs/notebooks.pbs
+# qsub -v NOTEBOOK=example_cadence_alt_sched_rolling_wfd.ipynb -N nb_alt_sched_rolling_wfd_Y10 jobs/notebooks.pbs
+# qsub -v NOTEBOOK=example_cadence_colossus_2667_wfd.ipynb -N nb_colossus_2667_wfd_Y10 jobs/notebooks.pbs
+# qsub -v NOTEBOOK=example_cadence_kraken_2035_wfd.ipynb -N nb_kraken_2035_wfd_Y10 jobs/notebooks.pbs
+# qsub -v NOTEBOOK=example_cadence_kraken_2042_wfd.ipynb -N nb_kraken_2042_wfd_Y10 jobs/notebooks.pbs
+# qsub -v NOTEBOOK=example_cadence_kraken_2044_wfd.ipynb -N nb_kraken_2044_wfd_Y10 jobs/notebooks.pbs
+# qsub -v NOTEBOOK=example_cadence_mothra_2049_wfd.ipynb -N nb_mothra_2049_wfd_Y10 jobs/notebooks.pbs
+# qsub -v NOTEBOOK=example_cadence_nexus_2097_wfd.ipynb -N nb_nexus_2097_wfd_Y10 jobs/notebooks.pbs
+# qsub -v NOTEBOOK=example_cadence_rolling_10yrs_opsim.ipynb -N nb_rolling_10yrs_opsim jobs/notebooks.pbs
+qsub -v NOTEBOOK=example_cadence_rolling_mix_10yrs_opsim.ipynb -N nb_rolling_mix_10yrs_opsim jobs/notebooks.pbs
+
+# example_cadence_alt_sched_rolling_wfd.ipynb
+# example_cadence_alt_sched_wfd.ipynb
+# example_cadence_colossus_2667_wfd.ipynb
+# example_cadence_kraken_2026_ddf.ipynb
+# example_cadence_kraken_2026_wfd.ipynb
+# example_cadence_kraken_2035_wfd.ipynb
+# example_cadence_kraken_2042_wfd.ipynb
+# example_cadence_kraken_2044_wfd.ipynb
+# example_cadence_mothra_2049_wfd.ipynb
+# example_cadence_nexus_2097_wfd.ipynb
+# example_cadence_pontus_2002_wfd.ipynb
+# example_cadence_pontus_2489_wfd.ipynb
+# example_cadence_rolling_10yrs_opsim.ipynb
+# example_cadence_rolling_mix_10yrs_opsim.ipynb
