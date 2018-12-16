274efd58e4341e23152b081ffffcc8f45576a31e
rlschuhmann <robert.schuhmann.13@ucl.ac.uk>
2018-12-16 03:15:51 +0000

    script for batch-processing feature extraction

--- utils/plasticc_extract_gp.py
+++ utils/plasticc_extract_gp.py
@@ -5,7 +5,7 @@ from astropy.table import Table
 import pickle
 import os,sys
 
-
+'''
 print('starting readin of monster files')
 #raw_data=pandas.read_csv('/share/hypatia/snmachine_resources/data/plasticc/test_set.csv')
 raw_data=pandas.read_csv('/share/hypatia/snmachine_resources/data/plasticc/training_set.csv')
@@ -15,59 +15,38 @@ raw_metadata=pandas.read_csv('/share/hypatia/snmachine_resources/data/plasticc/t
 print('read in metadata')
 sys.stdout.flush()
 
-objects=np.unique(raw_data['object_id'])
-filters=np.unique(raw_data['passband']).astype('str')
-
-
-out_folder='/home/roberts/data_sets/sne/plasticc/plasticc_training/'
-int_folder=os.path.join(out_folder,'int')
-feats_folder=os.path.join(out_folder,'features')
-
-d=sndata.EmptyDataset(filter_set=filters,survey_name='plasticc',folder=out_folder)
-
-for o,counter in zip(objects,range(len(objects))):
-	print('obj #%d: %s'%(counter,o))
-	sys.stdout.flush()
-	#slice data frame into one object/rest WITHOUT querying the entire frame
-	
-	linecounter=0
-	while linecounter < len(raw_data) and raw_data.iloc[linecounter]['object_id']==o:
-		linecounter+=1
-
-	fr=raw_data.iloc[:linecounter]
-	raw_data=raw_data.iloc[linecounter:]
-	
-
-	#assemble lightcurve table pertaining to one object
-	#fr=raw_data[raw_data['object_id']==o]
-
-	tab=Table([fr['mjd'],fr['passband'].astype('str'),fr['flux'],fr['flux_err']],names=['mjd','filter','flux','flux_error'])
-
-	tab['mjd']-=tab['mjd'].min()
-
-	tab.meta['name']=o.astype('str')
-	tab.meta['z']=raw_metadata['hostgal_specz'][raw_metadata['object_id']==o]
-
-	#insert into data set
-	d.insert_lightcurve(tab)
-
-with open(os.path.join(out_folder,'dataset.pickle'),'wb') as f:
-        pickle.dump(d,f)
+#objects=np.unique(raw_data['object_id'])
+#filters=np.unique(raw_data['passband']).astype('str')
 '''
+
+index=int(sys.argv[1])
+print('Performing feature extraction on batch %d'%index)
+
+out_folder='/share/hypatia/snmachine_resources/data/plasticc/data_products/plasticc_test/with_nondetection_cutting/fullset/data/'
 
 print('loading data')
 sys.stdout.flush()
-with open('/home/roberts/data_sets/sne/plasticc/plasticc_test/dataset_full.pickle','rb') as f:
+with open(os.path.join(out_folder,'dataset_%d.pickle'%index),'rb') as f:
 	d=pickle.load(f)
-'''
+int_folder=os.path.join(out_folder,'int')
+feats_folder=os.path.join(out_folder,'features')
 
-print('nobj: '+str(len(objects)))
+print('data loaded')
+sys.stdout.flush()
+#d=sndata.EmptyDataset(filter_set=filters,survey_name='plasticc',folder=out_folder)
+
+#d.object_names=d.object_names[:10]
+
+print('nobj: '+str(len(d.object_names)))
 print('extracting features')
 sys.stdout.flush() 
-wf=snfeatures.WaveletFeatures()
-feats=wf.extract_features(d,nprocesses=80,save_output='all',output_root=int_folder)
-feats.write(os.path.join(feats_folder, 'wavelet_features.fits'),overwrite=True)
+wf=snfeatures.WaveletFeatures(wavelet='sym2',ngp=1100)
 
+pca_folder='/share/hypatia/snmachine_resources/data/plasticc/dummy_pca/'
+
+feats=wf.extract_features(d,nprocesses=1,save_output='all',output_root=int_folder, recompute_pca=False, pca_path=pca_folder,xmax=1100)
+feats.write(os.path.join(feats_folder, 'wavelet_features_%d.fits'%index),overwrite=True)
+'''
 with open(os.path.join(feats_folder,'PCA_mean.pickle'),'wb') as f1:
 	pickle.dump(wf.PCA_mean,f1)
 with open(os.path.join(feats_folder,'PCA_eigenvals.pickle'),'wb') as f2:
@@ -75,7 +54,7 @@ with open(os.path.join(feats_folder,'PCA_eigenvals.pickle'),'wb') as f2:
 with open(os.path.join(feats_folder,'PCA_eigenvectors.pickle'),'wb') as f3:
         pickle.dump(wf.PCA_eigenvectors,f3)
 
-'''
+
 np.savetxt(os.path.join(feats_folder,'PCA_mean.txt'),wf.PCA_mean)
 np.savetxt(os.path.join(feats_folder,'PCA_eigenvals.txt'),wf.PCA_eigenvals)
 np.savetxt(os.path.join(feats_folder,'PCA_eigenvectors.txt'),wf.PCA_eigenvectors)
