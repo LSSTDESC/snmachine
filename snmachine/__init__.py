11210690220c347d38877f746a0171ae7ddeeb5a
R. Biswas <rbiswas4@gmail.com>
2019-03-25 17:59:21 +0100

    Fixing the version problem 	modified:   snmachine/__init__.py 	modified:   snmachine/version.py

--- snmachine/__init__.py
+++ snmachine/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import os
 import tarfile
 
+from .version import __VERSION__ as __version__
 here = __file__
 basedir = os.path.split(here)[0]
 example_data = os.path.join(basedir, 'example_data')
