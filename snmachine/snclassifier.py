1f442bb986984d8961dd881b7a12e3e1811e4c7b
Tarek Allam <t.allam.jr@gmail.com>
2019-04-29 13:27:08 +0100

    Removing setting of 'SEED' constant from snclassifer.py
    Instead placing this as an option to run_pipeline() function which a
    user may override or keep the default 1234 value

--- snmachine/snclassifier.py
+++ snmachine/snclassifier.py
@@ -35,7 +35,6 @@ try:
 except ImportError:
     print ('Neural networks not available in this version of scikit-learn. Neural networks are available from development version 0.18.')
 
-SEED = 1234
 
 def roc(pr, Yt, true_class=0):
     """
@@ -611,7 +610,8 @@ def __call_classifier(classifier, X_train, y_train, X_test, param_dict,return_cl
 
 def run_pipeline(features,types,output_name='',columns=[],classifiers=['nb','knn','svm','neural_network','boost_dt'],
                  training_set=0.3, param_dict={}, nprocesses=1, scale=True,
-                 plot_roc_curve=True,return_classifier=False,classifiers_for_cm_plots=[], type_dict = None):
+                 plot_roc_curve=True,return_classifier=False,classifiers_for_cm_plots=[],
+                 type_dict = None, seed=1234):
     """
     Utility function to classify a dataset with a number of classification methods. This does assume your test
     set has known values to compare against. Returns, if requested, the classifier objects to run on future test sets.
@@ -671,7 +671,7 @@ def run_pipeline(features,types,output_name='',columns=[],classifiers=['nb','knn
         #Split into training and validation sets
         if np.isscalar(training_set):
             objs=feats['Object']
-            objs=np.random.RandomState(seed=SEED).permutation(objs)
+            objs=np.random.RandomState(seed=seed).permutation(objs)
             training_set=objs[:(int)(training_set*len(objs))]
 
         #Otherwise a training set has already been provided as a list of Object names and we can continue
@@ -686,7 +686,7 @@ def run_pipeline(features,types,output_name='',columns=[],classifiers=['nb','knn
     else:
         #Otherwise the features are already in the form of a numpy array
         if np.isscalar(training_set):
-            inds=np.random.RandomState(seed=SEED).permutation(range(len(features)))
+            inds=np.random.RandomState(seed=seed).permutation(range(len(features)))
             train_inds=inds[:(int)(len(inds)*training_set)]
             test_inds=inds[(int)(len(inds)*training_set):]
 
