0b5af273587fd931a214db8d2e0503d9ec330c98
Catarina Alves <catarina._3@hotmail.com>
2021-07-16 18:14:44 +0100

    Going back

--- setup.py
+++ setup.py
@@ -36,11 +36,11 @@ class ExtractExampleData(install):
             print("the path should be: ", spcc_data)
 
             # Untar example data
-            #tar = tarfile.open(spcc_data + '.tar.gz')
             tar_path = os.path.join(PACKAGENAME, 'example_data','SPCC_SUBSET' )
             print("The new path is: ", tar_path)
             print(tar_path + '.tar.gz')
-            tar = tarfile.open(tar_path + '.tar.gz')  # try something new
+            tar = tarfile.open(spcc_data + '.tar.gz')
+            #tar = tarfile.open(tar_path + '.tar.gz')  # try something new
             print("I am untaring the file: ", tar_path + '.tar.gz')
             tar.extractall(example_data)
             print("I passed this yey!!")
