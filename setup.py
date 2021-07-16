040b243591b1edf1046830fd09fa725ca83ba2f2
Catarina Alves <catarina._3@hotmail.com>
2021-07-16 17:22:40 +0100

    Try to solve install problems

--- setup.py
+++ setup.py
@@ -24,14 +24,18 @@ class ExtractExampleData(install):
             paths = (site.getsitepackages(),)
             install_directory = paths[0][0]
             install_directory = os.path.join(install_directory, PACKAGENAME)
-
+	    
+	    print("I will find the data....")
             # Find example_data
             example_data = os.path.join(install_directory, 'example_data')
             spcc_data = os.path.join(example_data, 'SPCC_SUBSET')
+	    print("the path should be: ", spcc_data)
 
             # Untar example data
             tar = tarfile.open(spcc_data + '.tar.gz')
+	    print("I am untaring the file: ", spcc_data + '.tar.gz')
             tar.extractall(example_data)
+	    print("I passed this yey!!")
 
 
 setup(
@@ -45,6 +49,7 @@ setup(
     include_package_data=True,
     package_data={'snmachine': ['example_data/SPCC_SUBSET.tar.gz', 'example_data/output_spcc_no_z/features/*.dat', 'example_data/example_data_for_tests.pckl']},
     exclude_package_data={'utils': ['archive/*']},
+    cmdclass={'install': ExtractExampleData},
     url='https://github.com/LSSTDESC/snmachine',
     license='BSD-3-Clause License',
     author='Michelle Lochner',
