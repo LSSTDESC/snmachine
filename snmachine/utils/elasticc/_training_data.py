dfcc5af73d8207b60f7cf322906500e23017b226
aseyffert <9977207+aseyffert@users.noreply.github.com>
2025-06-04 20:27:59 +0200

    Streamline init machinery and structure of the ElasticcTrainingData class

--- snmachine/utils/elasticc/_training_data.py
+++ snmachine/utils/elasticc/_training_data.py
@@ -1,4 +1,5 @@
 from functools import partial
+from itertools import chain
 from multiprocessing import Pool
 from pathlib import Path
 from typing import Any, Callable, Generator, Iterator
@@ -9,9 +10,13 @@ from astropy.table import Table
 from numpy.typing import NDArray
 from tqdm import tqdm
 
-from ...sndata import default_pb_wavelengths
 from .._utils import are_sncosmo_aliases
-from ._training_metadata import BAND_LABELS
+from ._training_metadata import (
+    BAND_LABELS,
+    ALL_DATA_COLS,
+    ALL_SRC_CLASSES,
+    SRC_CLASS_TAXONOMY,
+)
 from ._typing import DataBundle, StrSpec, resolve_spec
 
 _read_table = partial(Table.read, character_as_bytes=False, memmap=False)
@@ -38,55 +43,41 @@ RENAMED_MDATA_COLS: dict[str, tuple] = dict(
     zip(["names", "new_names"], zip(*_renamed_mdata_cols.items()))
 )
 
+ALL_DATA_COLS_RENAMED = ALL_DATA_COLS - set(RENAMED_DATA_COLS["names"]) | set(
+    RENAMED_DATA_COLS["new_names"]
+)
+
 
 class ElasticcTrainingData:
-    SURVEY_NAME = "lsst"
-    FILTER_SET = tuple(default_pb_wavelengths[SURVEY_NAME])
-    from ._training_metadata import (
-        ALL_DATA_COLS,
-        ALL_METADATA_COLS,
-        ALL_SRC_CLASSES,
-        SRC_CLASS_TAXONOMY,
-    )
-
-    data_cols_key: dict[str, str] = {
-        "MJD": "mjd",
-        "BAND": "band",
-        "FLUXCAL": "flux",
-        "FLUXCALERR": "fluxerr",
-        "ZEROPT": "zp",
-    }
-    assert are_sncosmo_aliases(set(data_cols_key.values()))
-    data_cols_key |= {"PHOTFLAG": "detected", "ZEROPT_ERR": "zp_error"}
-    base_data_cols = set(data_cols_key.values())
-    derived_data_cols = {"days_since_detect", "src_class"}
-    assert set(data_cols_key.keys()).issubset(ALL_DATA_COLS)
+    data_cols_base = ["mjd", "band", "detected", "flux", "fluxerr", "zp", "zp_error"]
 
     def __init__(
         self,
         src_classes: StrSpec,
         root_dir: str | Path,
+        zeroed: bool = True,
         add_data_cols: StrSpec = "none",
+        add_src_class_col: bool = True,
         quiet_load: bool = False,
         **kwargs,
     ) -> None:
-        use_all: bool = isinstance(src_classes, str) and src_classes.lower() == "all"
-        self.src_classes: set[str] = (
-            self.ALL_SRC_CLASSES if use_all else self._parse_src_classes(src_classes)
-        )
+        self.src_classes: set[str] = self._parse_src_classes(src_classes)
         self.root_dir: Path = Path(root_dir) if isinstance(root_dir, str) else root_dir
         if not self.root_dir.is_dir():
             raise FileNotFoundError(f"Specified root_dir does not exist:\n{root_dir}")
-
-        drop_phot_cols: set[str] = self.ALL_DATA_COLS - (
-            set(self.data_cols_key.keys()) | self._parse_add_cols_spec(add_data_cols)
+        self.data_cols: set[str] = set(self.data_cols_base) | self._parse_add_cols_spec(
+            add_data_cols
         )
+
+        drop_phot_cols: set[str] = ALL_DATA_COLS_RENAMED - self.data_cols
 
         # FIXME: The len(thing) < 2 check(s) are missing for all the tqdm calls.
         _bundle = _load_training_data(
             self.src_classes,
             root_dir=self.root_dir,
+            zeroed=zeroed,
             drop_phot_cols=drop_phot_cols,
+            add_src_class_col=add_src_class_col,
             disable=quiet_load,
             **kwargs,
         )
@@ -98,61 +89,51 @@ class ElasticcTrainingData:
         return len(self.data)
 
     def _parse_src_classes(self, spec: StrSpec) -> set[str]:
-        if not isinstance(spec, set):
-            spec = {spec} if isinstance(spec, str) else set(spec)
-
-        src_classes: set[str] = spec & self.ALL_SRC_CLASSES
-        for spec_str in spec - self.ALL_SRC_CLASSES:
+        if isinstance(spec, str) and spec == "all":
+            return ALL_SRC_CLASSES
+        spec = resolve_spec(spec)
+        src_classes: set[str] = spec & ALL_SRC_CLASSES
+        for spec_str in spec - ALL_SRC_CLASSES:
             src_classes |= self._resolve_spec_str(spec_str.lower())
         return src_classes
 
     # TODO: Make these checks case insensitive.
     def _resolve_spec_str(self, spec_str: str) -> set[str]:
-        if spec_str in self.SRC_CLASS_TAXONOMY.keys():
-            supset_dict = self.SRC_CLASS_TAXONOMY[spec_str]
-            src_classes = set()
-            for supset in supset_dict.values():
-                src_classes |= supset
-            return src_classes
-        for supset_dict in self.SRC_CLASS_TAXONOMY.values():
-            if spec_str in supset_dict.keys():
+        if spec_str in SRC_CLASS_TAXONOMY:
+            return set(chain(*SRC_CLASS_TAXONOMY[spec_str]))
+        for supset_dict in SRC_CLASS_TAXONOMY.values():
+            if spec_str in supset_dict:
                 return supset_dict[spec_str]
-        else:
-            supset_keys = []
-            loop_keys = sorted(self.SRC_CLASS_TAXONOMY.keys())
-            for key in loop_keys:
-                supset_keys += sorted(self.SRC_CLASS_TAXONOMY[key].keys())
-            raise ValueError(
-                f"Invalid spec_str: '{spec_str}'. Value must be (equivalent to) 'all' or in:\n"
-                f"{loop_keys}\n"
-                f"or\n{supset_keys}\n"
-                f"or\n{sorted(self.ALL_SRC_CLASSES)}"
-            )
+        raise ValueError(
+            f"Invalid spec_str: '{spec_str}'. Value must be (equivalent to) 'all' or in:\n"
+            f"{sorted(SRC_CLASS_TAXONOMY)}\n"
+            f"or\n{sorted(chain(*SRC_CLASS_TAXONOMY.values()))}\n"
+            f"or\n{sorted(ALL_SRC_CLASSES)}"
+        )
 
     def _parse_add_cols_spec(self, add_cols: StrSpec) -> set[str]:
         if isinstance(add_cols, str):
-            add_cols = add_cols.lower()
-            if add_cols == "none":
-                return self.base_data_cols
-            elif add_cols == "all":
-                return self.ALL_DATA_COLS
-            else:
-                return self.base_data_cols & {add_cols}
-        if not isinstance(add_cols, set):
-            add_cols = set(add_cols)
+            if add_cols.lower() in set(self.data_cols_base) | {"none"}:
+                return set()
+            if add_cols.lower() == "all":
+                return ALL_DATA_COLS_RENAMED - set(self.data_cols_base)
+            if add_cols.upper() in ALL_DATA_COLS:
+                old_name, add_cols = add_cols, _renamed_data_cols[add_cols]
+                warn(f"{old_name} added to data cols but renamed to {add_cols}.")
+            if add_cols.lower() in ALL_DATA_COLS_RENAMED:
+                return {add_cols.lower()}
+        add_cols = resolve_spec(add_cols)
 
-        # TODO: Add check and warn for cols in data_cols_key.keys(); also kinda ignored.
-        bad_cols: set[str] = add_cols - (
-            self.ALL_DATA_COLS
-            | set(self.data_cols_key.values())
-            | self.derived_data_cols
-        )
-        if bad_cols:
+        assert isinstance(add_cols, set)
+        assert all(isinstance(val, str) for val in add_cols)
+
+        bad_cols: set[str]
+        if bad_cols := add_cols - (ALL_DATA_COLS | ALL_DATA_COLS_RENAMED):
             warn(
                 f"Ignoring invalid data column labels in add_data_cols:\n{bad_cols}\nValid labels are those in:\n"
-                f"{set(self.data_cols_key.values())}\nand/or\n{self.ALL_DATA_COLS}"
+                f"{sorted(ALL_DATA_COLS | ALL_DATA_COLS_RENAMED)}"
             )
-        return self.base_data_cols & add_cols - bad_cols
+        return add_cols - (bad_cols | set(self.data_cols_base))
 
 
 def _load_training_data(src_classes: set[str], **kwargs) -> DataBundle:
