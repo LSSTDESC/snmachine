42fe16f550fdf96db0b982e26e78d7e0869d7e06
aseyffert <9977207+aseyffert@users.noreply.github.com>
2025-06-04 22:00:01 +0200

    Streamline ALL_SNCOSMO_ALIASES's construction

--- snmachine/utils/_utils.py
+++ snmachine/utils/_utils.py
@@ -1,9 +1,8 @@
+from itertools import chain
 
 from sncosmo.photdata import PHOTDATA_ALIASES as SNCOSMO_ALIAS_KEY
 
-ALL_SNCOSMO_ALIASES = {
-    alias for aliases in SNCOSMO_ALIAS_KEY.values() for alias in aliases
-}
+ALL_SNCOSMO_ALIASES: set[str] = set(chain(*SNCOSMO_ALIAS_KEY.values()))
 
 
 def are_sncosmo_aliases(labels: set[str]) -> bool:
