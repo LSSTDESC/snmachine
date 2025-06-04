21d62b0011e1ec129a5a81c89d953a50eae3b4e6
aseyffert <9977207+aseyffert@users.noreply.github.com>
2025-06-04 20:28:36 +0200

    Add missing package-level constants for interactive environments

--- snmachine/utils/elasticc/__init__.py
+++ snmachine/utils/elasticc/__init__.py
@@ -1,4 +1,9 @@
 from .._utils import are_sncosmo_aliases, resolve_sncosmo_label
-from ._training_metadata import BAND_LABELS
+from ._training_metadata import (
+    BAND_LABELS,
+    ALL_DATA_COLS,
+    ALL_METADATA_COLS,
+    SRC_CLASS_TAXONOMY,
+)
 from ._training_data import BANDS_KEY, FNAME_TMPL, ElasticcTrainingData, load_src_class
 from ._typing import StrSpec, resolve_spec, DataBundle
