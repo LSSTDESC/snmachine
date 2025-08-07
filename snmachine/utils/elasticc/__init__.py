066e5bc54416de09b1759d82285c77dfe701b363
aseyffert <9977207+aseyffert@users.noreply.github.com>
2025-08-07 21:48:18 +0200

    Sort imports

--- snmachine/utils/elasticc/__init__.py
+++ snmachine/utils/elasticc/__init__.py
@@ -1,10 +1,10 @@
 from .training_data import TrainingData
 from .training_data.metadata import (
     ALL_DATA_COLS,
-    COMMON_METADATA_COLS,
-    MIXIN_METADATA_COLS,
-    DOOMED_METADATA_COLS,
     ALL_METADATA_COLS,
     ALL_SRC_CLASSES,
+    COMMON_METADATA_COLS,
+    DOOMED_METADATA_COLS,
+    MIXIN_METADATA_COLS,
     SRC_CLASS_TAXONOMY,
 )
